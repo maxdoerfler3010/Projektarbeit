37d74e2018d04ee9b12d5c7146216824d2b592aa
linakamm <5b8d4zz5m4@privaterelay.appleid.com>
2025-12-06 15:56:41 +0100

    boolean weg

--- src/main/java/Projektarbeit/Schuhe.java
+++ src/main/java/Projektarbeit/Schuhe.java
@@ -5,14 +5,12 @@ public class Schuhe {
     private int groesse;
     private double preis;
     private String material;
-    private boolean istWasserdicht;
 
-    public Schuhe(String marke, int groesse, double preis, String material, boolean istWasserdicht) {
+    public Schuhe(String marke, int groesse, double preis, String material) {
         this.marke = marke;
         this.groesse = groesse;
         this.preis = preis;
         this.material = material;
-        this.istWasserdicht = istWasserdicht;
     }
 
     public double brechnePreisNachlass (double prozent){
