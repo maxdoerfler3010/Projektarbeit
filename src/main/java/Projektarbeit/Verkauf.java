0d1969ee7dfa213fd5778b832ca600509d61b790
linakamm <5b8d4zz5m4@privaterelay.appleid.com>
2025-12-07 10:34:36 +0100

    GUI

--- src/main/java/Projektarbeit/Verkauf.java
+++ src/main/java/Projektarbeit/Verkauf.java
@@ -4,15 +4,15 @@ import javax.swing.*;
 
 public class Verkauf {
     private JPanel myPanel;
-    private JLabel Label_Marken;
-    private JLabel Label_Größe;
-    private JLabel Label_Preis;
+    private JLabel label_Marken;
+    private JLabel label_Größe;
+    private JLabel label_Preis;
     private JTextField tF_Preis;
     private JCheckBox checkBox_Wasserfest;
     private JList list1_Schuhliste;
-    private JLabel Label_Schuhliste;
-    private JButton button_hinzufügen;
-    private JButton button_löschen;
+    private JLabel label_Schuhliste;
+    private JButton speichernButton;
+    private JButton löschenButton;
     private JComboBox comBox_Marken;
     private JTextField tF_Größe;
 }
