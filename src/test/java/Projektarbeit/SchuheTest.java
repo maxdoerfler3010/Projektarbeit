9f2491b98442c4ca293c2d5474da0040062b8636
linakamm <5b8d4zz5m4@privaterelay.appleid.com>
2025-12-17 10:14:56 +0100

    Test erstellt

--- src/test/java/Projektarbeit/SchuheTest.java
+++ src/test/java/Projektarbeit/SchuheTest.java
@@ -6,6 +6,14 @@ class SchuheTest {
 
     @org.junit.jupiter.api.Test
     void applyDiscount() {
+        // Arrange (Vorbereiten)
+        Schuhe schuhe = new Schuhe("Nike", 40, 100, true);
+
+        // Act (Methode ausführen)
+        schuhe.applyDiscount(0.8); // 20 % Rabatt
+
+        // Assert (Ergebnis prüfen)
+        assertEquals(80.0, schuhe.getPreis());
 
     }
 }
